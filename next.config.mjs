5a077861a27961f45dc7f2daec37e3425a2df23d
CheSebas <rodulfsebas@hotmail.com>
2025-08-31 22:00:44 -0500

    Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
 
-export default nextConfig;
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/Nivel_2-Practica-4" : "",
+  assetPrefix: isProd ? "/Nivel_2-Practica-4/" : "",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
